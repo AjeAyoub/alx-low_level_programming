2c7090754cf900f9a5590bec60adcc6fa4381bf1
AjeAyoub <ajeddabayoub.official@gmail.com>
2023-03-27 19:19:09 -0700

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: teh string
+ * Return: the length of the string
  */
-int main(void)
-{
-    char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
 }

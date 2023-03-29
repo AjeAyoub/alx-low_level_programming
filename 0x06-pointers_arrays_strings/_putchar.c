1789229d1a39153617618350a768f35bac5b666e
AjeAyoub <ajeddabayoub.official@gmail.com>
2023-03-29 09:48:32 -0700

    More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: char to be printed
+ * Return: i on success
+ */
+int _putchar(char c)
+{
+	return (writes(1, &c, 1));
+}
+

a4846dc22870d96211c89e7ad76386253bccff08
AjeAyoub <ajeddabayoub.official@gmail.com>
2023-05-08 15:39:22 -0700

    function that appends text at the end of a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *
+ * Otherwise - 1.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, ln = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (ln = 0; text_content[ln];)
+			ln++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, ln);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
+

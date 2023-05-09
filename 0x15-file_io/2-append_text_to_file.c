4483b49860442f53f116e9cabfcabf3b6206d640
AjeAyoub <ajeddabayoub.official@gmail.com>
2023-05-08 17:44:06 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,17 +1,14 @@
 #include "main.h"
-
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
- *
- * Return: If the function fails or filename is NULL - -1.
- *         If the file does not exist the user lacks write permissions - -1.
- *         Otherwise - 1.
- */
+*append_text_to_file - Appends text to the end of a file.
+*@filename: A pointer to the name of the file to append to.
+*@text_content: A pointer to the text to append.
+*Return: On success - 1.
+*On failure - (-1).
+*/
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, ln = 0;
+	int o, wr, ln = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -23,7 +20,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, ln);
+	wr = write(o, text_content, ln);
 
 	if (o == -1 || w == -1)
 		return (-1);
